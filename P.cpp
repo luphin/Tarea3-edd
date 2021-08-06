49c33ee2b766242c24d031bbb61094840c617f37
luphin <sebaszegarra@gmail.com>
2021-08-06 01:08:40 -0400

    cambios 6/agosto 1:08 am

--- P.cpp
+++ P.cpp
@@ -110,35 +110,38 @@ int main(){
             tipo_compras[contador].amount++;
         }
         int val_a_pagar = 0;
-        int descuento = 0;
+        int descuentototal; = 0;
         int render[tipo_productos];
-        for (int a = 0 ; a < tipo_productos ; a++){
+        for (int a = 0 ; a < tipo_productos ; a++){ //solo la suma de todos los productos, dado que pueden tener distintos precios
             //Aqui va el calculo del valor, comprobando por equivalentes, y sumar el precio a val_a_pagar
-            Nodo val_1 = tipo_compras[a];
-            int flag = 1; //No esta
+            Nodo val_1 = tipo_compras[a];           //almacena el nodo en la posicion a de tip_comrpas
             int cantidadtotal = 0 ;
             val_a_pagar =  val_a_pagar + (hashSearchProducto(hash_Productos ,val_1.code).precio * val_1.amount);
-            
+        }
+        for (int a = 0 ; a < tipo_productos ; a++){  //calculamos el descuento, dado que pueden tener los mismos descuentos un producto con otro, de esta forma no se revisa 2 veces el mismo porducto
             /*
+            Nodo val_1 = tipo_compras[a]; //almacena el nodo en la posicion a de tipo_comrpas
+            int cantidadtotal = 0 ;
             cantidadtotal = cantidadtotal + val_1.amount(); //variable para almacenar la cantidad de productos totales, al fin y al cabo tiene el mismo valor de descuento con los equivalentes
 
             oferta flash = hashInsertOfertas(hash_Ofertas, val_1.code) //almacena el struck ofecta, para asi tenerlo a mano 
-            //for(int a = 0; a< sizeof(flash.productos_equivalentes)
             for(int w = 0; w< 10; w++){ //iteramos dentro de los productos que son equivalentes
                 if(flash.productos_equivalentes[w] == -1){  //si es igual a -1 se termina porque los demas van a ser -1 igualemnete
+                    descuentototal = descuentototal + (int(cantidatotal % flash.cantidad_descuento) * flash.descuento); //se calcula el descuento total de los equivalentes antes de forzar el cierre del for
                     break;
                 }
                 for (int t= 0; t < tipo_productos; t++){  //para iterar dentro de tipo_compras y asi descontar de una los productos que son equivalentes
                     if(flash.productos_equivalentes[w] == tipo_compras[t].code){ 
                         cantidadtotal = cantidadtotal + tipo_compras[t].amount; //suma el valor de los productos que se encuentren
                         for(contador = t; contador < tipo_productos; contador++){  //eleminar el struck de tipo_compras, asi se aplica el descuento de una
-			                strcpy(tipo_compras[contador], tipo_compras[contador + 1]);	\\ NO ESTOY SEGURO SI FUNCIONA, borra el struck del tipo_compras
+			                strcpy(tipo_compras[contador], tipo_compras[contador + 1]);	// NO ESTOY SEGURO SI FUNCIONA, borra el struck del array tipo_compras
 		                }
                     }
                 }
             }
             */
         }
+        val_a_pagar = val_a_pagar - descuentototal; //calcula el total, valor total - descuento
         boletas << val_a_pagar;
     }
     compras.close();
